9bb4c4b98ffa125737681328307dfb49ea36a5ff
andrianm28 <andrianmaulana28@gmail.com>
2019-07-07 19:54:50 +0700

    migrasi storage

--- app/src/main/java/com/qtasnim/qhopes/navigation/fragments/MyappointmentNavFragment.java
+++ app/src/main/java/com/qtasnim/qhopes/navigation/fragments/MyappointmentNavFragment.java
@@ -6,75 +6,60 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.qtasnim.qhopes.R;
 import com.qtasnim.qhopes.activities.MainActivity;
+import com.qtasnim.qhopes.fragments.UpcomingAppointmentFragment;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MyappointmentNavFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MyappointmentNavFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyappointmentNavFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private OnFragmentInteractionListener mListener;
 
     public MyappointmentNavFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyappointmentNavFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MyappointmentNavFragment newInstance(String param1, String param2) {
         MyappointmentNavFragment fragment = new MyappointmentNavFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View rootView = inflater.inflate(R.layout.fragment_nav_myappointment, container, false);
         ((AppCompatActivity)getActivity()).getSupportActionBar().show();
         // Set title bar
         ((MainActivity) getActivity())
                 .setActionBarTitle(getString(R.string.title_myappointment));
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_myappointment, container, false);
+
+        ToggleButton mUpcoming = rootView.findViewById(R.id.btn_upcoming_appointment);
+
+        mUpcoming.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UpcomingAppointmentFragment fragment2 = new UpcomingAppointmentFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame_appointment, fragment2);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
